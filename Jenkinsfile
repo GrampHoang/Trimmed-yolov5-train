fb1946c651c77b3a14dea3e00e9e43cd34fef513
chih <hoang.chiengphi@elca.vn>
2023-03-31 18:13:04 +0700

    Upload model to repos

--- Jenkinsfile
+++ Jenkinsfile
@@ -29,6 +29,7 @@ pipeline {
     }
 
     parameters {
+        string(name: 'MODEL_NAME', description: 'The name for the model')
         string(name: 'IMG', description: 'The image size for training. Example 480', defaultValue: "480")
         string(name: 'BATCH', description: 'The number to build at a time. Example 1', defaultValue: "1")
         string(name: 'EPOCH', description: 'The number of training for model. Example 1', defaultValue: "1")
@@ -44,7 +45,7 @@ pipeline {
     environment {
         // Copy the Jenkins build number of Suite-Build job into a global iPension environment variable
         MLOPS_TRAIN_NUMBER = "${env.BUILD_NUMBER}"
-        ARCHIV = 'runs_'+"${params.DATA_PATH}"+"${MLOPS_TRAIN_NUMBER}"+'.tar.gz'
+        ARCHIV = "${params.MODEL_NAME}"+'.tar.gz'
         // Define default job parameters
         propagate = true
 
@@ -52,7 +53,7 @@ pipeline {
 
     stages {
         
-        stage('Initial data') {
+        stage('Initial training data') {
             steps {
                 sh '''
                     python --version
@@ -87,16 +88,16 @@ pipeline {
         stage("Push training result to model store"){
             steps {
                 script {
-                    sh "python store/insertFile.py ${ARCHIV}"
+                    sh "ls"
+                    sh "python store/insertFile.py --nodelName ${params.MODEL_NAME} --weightFilePath runs/${ARCHIV} --img ${params.IMG} --batch ${params.BATCH} --epochs ${params.EPOCH}  --version ${MLOPS_TRAIN_NUMBER}"
                 }
             }
         }
 
     }
-        post {
-            always {
-                cleanWs()
-            }
+    post {
+        always {
+            cleanWs()
         }
-
+    }
 }
--- store/GetFile.py
+++ store/GetFile.py
@@ -1,21 +1,48 @@
 import pymongo
 import sys
 import os
-# from bson.binary import Binary
-# from datetime import datetime
+from bson.binary import Binary
+from datetime import datetime
 import shutil
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# python GetFile.py --outputFolderPath output --modelName testmodel --version 2.1
+# outputFolderPath là cái path để tạo cái folder, sau đó nó sẽ để 3 cái file với model name tương ứng vào
+parser.add_argument("--outputFolderPath", help="Add path you want to create folder to put weight file in")
+parser.add_argument("--modelName", help="Add model name you want to get")
+parser.add_argument("--version", help="Add version")
+
+args = parser.parse_args()
 
 client = pymongo.MongoClient(
     "mongodb+srv://haicauancarem:tiachop1@cluster0.dd88nyj.mongodb.net/?retryWrites=true&w=majority")
 mydb = client["MLOpsData"]
 mycol = mydb["weight"]
 
+outputFolderPath = args.outputFolderPath
 
-outputFolderPath = sys.argv[1]
 if os.path.exists(outputFolderPath) and os.path.isdir(outputFolderPath):
     shutil.rmtree(outputFolderPath)
-for x in mycol.find():
-  filename = x["fileName"]
-  os.makedirs(os.path.dirname(outputFolderPath + "/" + str(x["binDate"]) +"/" + filename), exist_ok=True)
-  with open(outputFolderPath + "/" + str(x["binDate"]) +"/" + filename, "wb") as f:
-    f.write(x["file"])
+
+modelName = args.modelName
+version = args.version
+
+# for x in mycol.find():
+#   filename = x["fileName"]
+#   os.makedirs(os.path.dirname(outputFolderPath + "/" + str(x["binDate"]) +"/" + filename), exist_ok=True)
+#   with open(outputFolderPath + "/" + str(x["binDate"]) +"/" + filename, "wb") as f:
+#     f.write(x["file"])
+
+y = mycol.find({'modelName': modelName, 'version': version}).sort("version", 1)
+x = None
+for element in y:
+    x = element
+if x:
+    weightFileName = x["weightFileName"]
+    os.makedirs(os.path.dirname(outputFolderPath +"/" + weightFileName), exist_ok=True)
+    with open(outputFolderPath +"/" + weightFileName, "wb") as f:
+        f.write(x["weightFile"])
+else:
+    raise Exception("Sorry, there is no model with that name")
