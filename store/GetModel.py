1f131256bb7a563dfb4897c4619f0e3c7459ff03
trivm2472@gmail.com <trivm2472@gmail.com>
2023-05-12 20:56:10 +0700

    add mongodb store file

---
+++ store/GetModel.py
@@ -0,0 +1,73 @@
+import pymongo
+import sys
+import os
+from bson.binary import Binary
+from datetime import datetime
+import shutil
+import argparse
+import gridfs
+
+parser = argparse.ArgumentParser()
+
+
+# python GetModel.py --modelName testmodel
+# Ket qua luu trong file versionList.json
+
+
+parser.add_argument("--modelName", help="Add model name you want to get")
+parser.add_argument("--version", help="Add version")
+
+args = parser.parse_args()
+
+client = pymongo.MongoClient(
+    "mongodb+srv://haicauancarem:tiachop1@cluster0.dd88nyj.mongodb.net/?retryWrites=true&w=majority")
+mydb = client["MLOpsData"]
+mycol = mydb["weight"]
+# fs = gridfs.GridFS(mydb)
+
+# outputFolderPath = args.outputFolderPath
+
+# if os.path.exists(outputFolderPath) and os.path.isdir(outputFolderPath):
+#     shutil.rmtree(outputFolderPath)
+
+modelName = args.modelName
+# version = args.version
+
+# for x in mycol.find():
+#   filename = x["fileName"]
+#   os.makedirs(os.path.dirname(outputFolderPath + "/" + str(x["binDate"]) +"/" + filename), exist_ok=True)
+#   with open(outputFolderPath + "/" + str(x["binDate"]) +"/" + filename, "wb") as f:
+#     f.write(x["file"])
+
+y = mycol.find({'modelName': modelName}).sort("id", -1)
+
+versionArray = []
+for element in y:
+    versionArray += [element["version"]]
+
+import json
+
+data = {"version_array": versionArray}
+
+# write data to a JSON file
+with open("versionList.json", "w") as f:
+    json.dump(data, f)
+
+# read data from the JSON file
+with open("versionList.json", "r") as f:
+    data = json.load(f)
+
+
+# print(os.environ['VERSION_ARRAY'])
+# x = None
+# for element in y:
+#     x = element
+# if x:
+#     gridID = x["gridID"]
+#     weightFileName = x["weightFileName"]
+#     temp = fs.get(gridID).read()
+#     os.makedirs(os.path.dirname(outputFolderPath +"/" + weightFileName), exist_ok=True)
+#     with open(outputFolderPath +"/" + weightFileName, "wb") as f:
+#         f.write(temp)
+# else:
+#     raise Exception("Sorry, there is no model with that name")
---
+++ store/InsertModel.py
@@ -0,0 +1,75 @@
+import pymongo
+# import base64
+# import bson
+import sys
+import os
+from bson.binary import Binary
+from datetime import datetime
+import argparse
+import gridfs
+import yaml
+import json
+
+
+# py InsertModel.py --resultFilePath opt.yaml --modelName testmodel --img 640 --batch 20 --epochs 6 --version 2.1
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--resultFilePath",
+                    help="Add path to opt.yaml file")  
+parser.add_argument("--modelName", help="Add model name")
+parser.add_argument("--img", help="Add image size")
+parser.add_argument("--batch", help="Add batch")
+parser.add_argument("--epochs", help="Add epoch")
+
+parser.add_argument("--version", help="Add version")
+
+args = parser.parse_args()
+
+
+client = pymongo.MongoClient(
+    "mongodb+srv://haicauancarem:tiachop1@cluster0.dd88nyj.mongodb.net/?retryWrites=true&w=majority")
+mydb = client["MLOpsData"]
+mycol = mydb["weight"]
+myid = mydb["seqs"]
+# fs = gridfs.GridFS(mydb)
+
+id = myid.find_one()["id"]
+
+now = datetime.now()
+binDate = now.year*10000000000 + now.month * 100000000 + \
+    now.day * 1000000 + now.hour*10000 + now.minute*100 + now.second
+current_time = now.strftime("%d/%m/%Y-%H:%M:%S")
+
+resultFilePath = args.resultFilePath
+img = args.img
+batch = args.batch
+epochs = args.epochs
+
+
+modelName = args.modelName
+version = args.version
+
+# gridID = fs.put(open(resultFilePath, 'rb'))
+with open(resultFilePath, "r") as f:
+    configuration = yaml.safe_load(f)
+
+with open('config.json', 'w') as json_file:
+    json.dump(configuration, json_file)
+
+
+output = json.dumps(json.load(open('config.json')), indent=2)
+os.remove("config.json")
+
+# y = mycol.find({"modelName": modelName}, {
+#                "modelName": 1, "version": 1}).sort("version", 1)
+
+
+
+x = mycol.insert_one({"id": id,
+                      "resultFile": output,
+                      "date": current_time, "binDate": binDate, "modelName": modelName,
+                      "img": img, "batch": batch, "epochs": epochs,
+                      "version": version, "deployed": False })
+
+myid.find_one_and_update({}, {'$set': {"id": id + 1}})
